7c1c178560e46a3fd928b19b748540c05eb2eb1e
Liz Bayardelle <lizbayardelle@gmail.com>
2025-11-14 15:24:45 -0800

    IMPROVE: Homepage UI/UX optimizations - Remove jQuery, newsletter, add lazy loading, improve queries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @latest_blog = Blog.where(published: true).order("created_at DESC").limit(1).first
-    @videos = Blog.where.not(video_url: "").limit(12)
+    @videos = Blog.where.not(video_url: "").order(created_at: :desc).limit(12)
   end
 end
